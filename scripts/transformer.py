845714e3937c57a003a3aaa76152db8de6a46738
Rishab <rishab.jituri@gmail.com>
2024-10-07 15:03:21 -0400

    attention

--- scripts/transformer.py
+++ scripts/transformer.py
@@ -4,6 +4,12 @@ import torch.nn.functional as F
 import math
 
 
+class LayerNorm(nn.Module):
+    def forward():
+        
+        
+
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, input_size:int, input_dim:int, heads:int, param_dim:int, final_dim):
         super().__init__()
@@ -11,15 +17,15 @@ class MultiHeadAttention(nn.Module):
         self.input_dim = input_dim
         self.heads = heads
         self.Qw = torch.rand([heads,input_size.size(-1),param_dim])
-        self.Vw = torch.rand([heads,input_size.size(-1),param_dim])
         self.Kw = torch.rand([heads,input_size.size(-1),param_dim])
+        self.Vw = torch.rand([heads,input_size.size(-1),input_dim//heads])
         self.final_linear = nn.Linear(param_dim*heads,final_dim)
         
     def forward(self,x):
         Q = torch.matmul(x,self.Qw)
         K = torch.matmul(x,self.Kw)
         V = torch.matmul(x,self.Vw)
-        score = F.sigmoid(Q@K.transpose(1,2)/K.size(-1))
+        score = F.sigmoid(Q@K.transpose(1,2)/math.sqrt(K.size(-1)))
         scale_dot_attention = torch.matmul(score,V)
         concatenated = scale_dot_attention.reshape(45,-1)
         final = self.final_linear(concatenated)
@@ -29,6 +35,10 @@ class Encoder(nn.Module):
     def __init__(self,input_size,embedding_dim,attention_heads,attention_param_dim,attention_linear_dim):
         super().__init__()
         self.attention = MultiHeadAttention(input_size,embedding_dim,attention_heads,attention_param_dim,attention_linear_dim)
+    
+    def forward(self,x):
+        attention_output = self.attention(x)
+        
         
            
    
